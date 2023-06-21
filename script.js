380ba2546ee076c3117959bd929db18c120302de
unixnexo <unixnexo@gmail.com>
2023-06-22 01:15:38 +0330

    dont know what im doing

--- script.js
+++ script.js
@@ -46,3 +46,67 @@ const typingEffect = () => {
 };
 
 typingEffect();
+
+
+/********** typing effect in our work ************/
+const words1 = ["پروژه های", "موفقیت های", "نمونه کارهای"];
+let wordIndex1 = 0;
+let charIndex1 = 0;
+let isDeleting1 = false;
+
+const typingEffect1 = () => {
+  const changingWordElement1 = document.getElementById("changing-word1");
+  const currentWord1 = words1[wordIndex1];
+
+  if (isDeleting1) {
+    changingWordElement1.textContent = currentWord1.substring(0, charIndex1 - 1);
+    charIndex1--;
+  } else {
+    changingWordElement1.textContent = currentWord1.substring(0, charIndex1 + 1);
+    charIndex1++;
+  }
+
+  if (!isDeleting1 && charIndex1 === currentWord1.length) {
+    isDeleting1 = true;
+    setTimeout(typingEffect1, 1000);
+  } else if (isDeleting1 && charIndex1 === 0) {
+    isDeleting1 = false;
+    wordIndex1 = (wordIndex1 + 1) % words1.length;
+    setTimeout(typingEffect1, 500);
+  } else {
+    setTimeout(typingEffect1, 100);
+  }
+};
+
+typingEffect1();
+
+
+/********** carousel in our work section ************/
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Array of text to be displayed
+  const textArray = [
+    "Text Goes Here",
+    "Another Text Example",
+    "More Text to Slide",
+  ];
+  
+  const slideTextElement = document.getElementById("slideText");
+  let currentIndex = 0;
+
+  // Function to update the text
+  function updateText() {
+    slideTextElement.textContent = textArray[currentIndex];
+    currentIndex = (currentIndex + 1) % textArray.length;
+  }
+
+  // Initial text update
+  updateText();
+
+  // Listen for animation end event and update the text
+  slideTextElement.addEventListener("animationend", updateText);
+});
+
+
+
+
