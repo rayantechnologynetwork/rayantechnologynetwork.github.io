f495283f76ac0283cbaa8637d84089f69af16653
beatrix <beatrixik@hotmail.com>
2023-06-21 17:59:13 +0330

    sucker

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// const sentence = "The weather is";
-const words = ["طراحی وب سایت", "فروشگاه وردپرسی", "awesome", "excellent"];
+/********** typing effect main con ************/
+const text = `شرکت رایان تکنولوژی نتورک، یک شرکت فعال در حوزه خدمات فناوری اطلاعات است. ما خدمات گسترده ای را در زمینه طراحی وب سایت، تولید محتوا، شبکه، امنیت و سئو ارائه می‌دهیم. 
+تیم ما از افراد جوان در این حوضه تشکبل شده و تمام تلاش ما برای آسان کردن کارهای پیچیده ی شما است.`;
+const element = document.getElementById("--abutUsText");
+let index = 0;
+
+function typeEffect() {
+  if (index < text.length) {
+    element.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, 20); 
+  }
+}
+
+typeEffect();
+
+
+/********** typing effect in why us ************/
+const words = ["ما", "شرکت ما", "سروریس های ما"];
 let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
@@ -29,20 +46,3 @@ const typingEffect = () => {
 };
 
 typingEffect();
-
-
-
-
-const text = `شرکت رایان تکنولوژی نتورک، یک شرکت فعال در حوزه خدمات فناوری اطلاعات است. ما خدمات گسترده ای را در زمینه طراحی وب سایت، تولید محتوا، شبکه، امنیت و سئو ارائه می‌دهیم. تیم ما از افرادی با تجربه و متخصص در این حوزه تشکیل شده است و با استفاده از دانش و تکنولوژی‌های روز، بهترین راهکارها را برای مشتریانمان ارائه می‌دهیم. هدف ما ایجاد تجربه کاربری برتر و افزایش نوآوری در حوزه فناوری اطلاعات است. با رایان تکنولوژی نتورک، ما به شما کمک می‌کنیم تا بهترین نتیجه را در دنیای دیجیتال بدست آورید.`;
-const element = document.getElementById("--abutUsText");
-let index = 0;
-
-function typeEffect() {
-  if (index < text.length) {
-    element.innerHTML += text.charAt(index);
-    index++;
-    setTimeout(typeEffect, 20); // Adjust typing speed here
-  }
-}
-
-typeEffect();
